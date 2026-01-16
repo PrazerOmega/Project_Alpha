50ceefe32714f6a4a3528923a685f6a48741f2a3
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2026-01-16 16:52:26 -0300

    First_Upload

---
+++ include/task_structs.h
@@ -0,0 +1,23 @@
+#ifndef TASK_STRUCT_H
+#define TASK_STRUCT_H
+
+#include <stdbool.h>
+
+typedef struct
+{
+    unsigned int id;
+    char *title;
+    char *description;
+    bool completed;
+}Task;
+
+typedef struct
+{
+    Task *pTask;
+    unsigned int count;
+    int capacity;
+
+}TaskList;
+
+
+#endif TASK_STRUCT_H
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "file_io.h"
+#include "log.h"
+#include "memory_utils.h"
+#include "task_manager.h"
+#include "task_structs.h"
+
+int main(void){
+
+
+
+
+    return 0;
+}
