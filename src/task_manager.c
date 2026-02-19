0d4fd9c376f21755e48077e74e9c061afb188499
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2026-02-19 18:55:57 -0300

    CHANGING TASK MANAGER.C
    Finishing the add_task function

--- src/task_manager.c
+++ src/task_manager.c
@@ -6,18 +6,30 @@
 #include "task_manager.h"
 #include "memory_utils.h"
 
-void add_task(TaskList *list, Task new_task){
+void add_task(TaskList *list, Task newTask){
+
+int taskID = list->count+1;
+
 if (list == NULL)
 {
     perror("Lista nao localizada");
 }
 if (list->count == list->capacity)
 {
-    printf("Espaco para tarefas quase cheio, aumentando espaco...");
-    safe_reallloc(new_task,list->capacity+10);
+    printf("Espaco para tarefas quase cheio, aumentando espaco...\n");
+    safe_reallloc(list,10);
+    printf("Espaco novo realocado\n");
 }
 
-    
+
+printf("Adicionando nova tarefa a lista");
+
+    list->pTask->id = taskID;
+    strcpy(list->pTask->title, newTask.title);
+    strcpy(list->pTask->description, newTask.description);
+    list->pTask->completed = 0;
+
+    list =+1;
 
     
     
